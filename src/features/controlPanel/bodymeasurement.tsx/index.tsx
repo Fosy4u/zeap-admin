7e25fcf03cdb266afe5f1a4c5504d870baef8a6b
foster <foster.ogwudu@yahoo.com>
2025-01-25 20:20:42 +0000

    fix body for mobile

--- src/features/controlPanel/bodymeasurement.tsx/index.tsx
+++ src/features/controlPanel/bodymeasurement.tsx/index.tsx
@@ -2,28 +2,38 @@ import { NavLink } from 'react-router-dom';
 
 const BodyMeasurement = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-6 justify-center w-full items-center h-screen">
-      <NavLink
-        to="/control-panel/body-measurement/male"
-        className={`w-85 md:w-100 rounded-md shadow-md border p-4 px-2 md:px-4  hover:bg-lightGold dark:hover:bg-darkGold  transition duration-300`}
-      >
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <span className="text-xl font-bold text-darkGold dark:text-lightGold">
-            Male
-          </span>
+    <>
+      <div className="flex justify-between md:items-center md:justify-between mb-8 p-4 bg-white dark:bg-boxdark  rounded-lg shadow  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div>
+          {' '}
+          <h1 className="text-xl md:text-2xltext-dark">
+            Body Measurement Guide
+          </h1>
         </div>
-      </NavLink>
-      <NavLink
-        to="/control-panel/body-measurement/female"
-        className={`w-85 md:w-100  rounded-md shadow-md border p-4 px-2 md:px-4 hover:bg-lightGold dark:hover:bg-darkGold transition duration-300`}
-      >
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <span className="text-xl font-bold text-darkGold dark:text-lightGold">
-            Female
-          </span>
-        </div>
-      </NavLink>
-    </div>
+      </div>
+      <div className="flex flex-col md:flex-row gap-6 justify-center w-full md:items-center h-[30rem] md:h-screen">
+        <NavLink
+          to="/control-panel/body-measurement/male"
+          className={`w-85 md:w-100 rounded-md shadow-md border p-4 px-2 md:px-4  hover:bg-lightGold dark:hover:bg-darkGold  transition duration-300`}
+        >
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <span className="text-xl font-bold text-darkGold dark:text-lightGold">
+              Male
+            </span>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/control-panel/body-measurement/female"
+          className={`w-85 md:w-100  rounded-md shadow-md border p-4 px-2 md:px-4 hover:bg-lightGold dark:hover:bg-darkGold transition duration-300`}
+        >
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <span className="text-xl font-bold text-darkGold dark:text-lightGold">
+              Female
+            </span>
+          </div>
+        </NavLink>
+      </div>
+    </>
   );
 };
 
